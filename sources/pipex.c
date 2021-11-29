688e5221dad2c44974cf59f05278083bdaa93535
EGAT <egacebo@yahoo.com>
2021-11-29 20:05:34 +0100

    Pipex - 29-11-21 20:05

--- sources/pipex.c
+++ sources/pipex.c
@@ -6,7 +6,7 @@
 /*   By: egomez-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 14:42:20 by egomez-a          #+#    #+#             */
-/*   Updated: 2021/11/29 18:12:57 by egomez-a         ###   ########.fr       */
+/*   Updated: 2021/11/29 20:03:47 by egomez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,52 +23,58 @@ void	start_child_1(int *fd, char **argv, t_pipex pipex, char **envp)
 {	
 	int		fd_infile;
 
+	printf("Entrando en el child 1\n");
 	fd_infile = open(argv[1], O_RDONLY);
+	printf("Abierto el infile con fd = %d\n", fd_infile);
 	check_fd(fd_infile, argv[1]);
+	close(fd[FD_READ_END]);
 	dup2(fd_infile, STDIN_FILENO);
+	printf("El STDIN_FILENO es %d", STDIN_FILENO);
 	close(fd_infile);
 	dup2(fd[FD_WRITE_END], STDOUT_FILENO);
-	close(fd[FD_WRITE_END]);
+	printf("Saliendo del proceso hijo1\n");
 	if (execve(pipex.cmd1[0], pipex.cmd1, envp) == -1)
 	{
 		perror("Could not execve cmd 1");
 		freepointers(pipex);
 		exit (errno);
 	}
+	close(fd[FD_WRITE_END]);
 }
 
 void	start_child_2(int *fd, char **argv, t_pipex pipex, char **envp)
 {
 	int		fd_outfile;
-	
-	close(fd[FD_WRITE_END]);
+
+	printf("Entrando en el child 2\n");
 	fd_outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	printf("Outfile creada con fd = %d\n", fd_outfile);
 	if (fd_outfile < 0)
-	{
-		perror("Error creating or opening outfile\n");
-		exit (errno);
-	}
+		printf("Error creating or opening outfile\n");
 	dup2(fd[FD_READ_END], STDIN_FILENO);
 	close(fd[FD_READ_END]);
 	dup2(fd_outfile, STDOUT_FILENO);
 	if (execve(pipex.cmd2[0], pipex.cmd2, envp) == -1)
 	{
-    	perror("Could not execve cmd 2");
+		perror("Could not execve cmd 2");
 		freepointers(pipex);
 		exit (errno);
 	}
+	close(fd[FD_READ_END]);
+	printf("Saliendo del proceso hijo2\n");
 }
 
 void	child_2_process(int *fd, int pid, t_pipex pipex, char **argv, char **envp)
 {
-	close(fd[FD_WRITE_END]);
+	printf("****Voy a generar el segundo proceso\n");
 	pid = fork();
+	printf("    el número pid del proceso Hijo2 generado es %d\n", pid);
 	if (pid < 0)
 	{
 		perror("Fork error. Child not created. \n");
 		exit (errno);
 	}
-	if(pid == 0)
+	if (pid == 0)
 		start_child_2(fd, argv, pipex, envp);
 	else
 	{
@@ -77,18 +83,19 @@ void	child_2_process(int *fd, int pid, t_pipex pipex, char **argv, char **envp)
 	}
 }
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-    int			fd[2];
+	int			fd[2];
 	pid_t		pid;
 	t_pipex		pipex;
-	int 		status;
-	int 		*check;
-	
-	printf("El numero de argumentos es %d\n", argc);
+	int			status;
+	int			*check;
+
 	check_entry(argc);
 	pipex.cmd1 = ft_split(argv[2], ' ');
 	pipex.cmd2 = ft_split(argv[3], ' ');
+	printf("Command 1 es %s %s\n", pipex.cmd1[0], pipex.cmd1[1]);
+	printf("Command 1 es %s %s\n", pipex.cmd2[0], pipex.cmd2[1]);
 	pipex.path = env_variable(envp);
 	check = check_cmd_path(pipex);
 	if (check[0] == 0 || check[1] == 0)
@@ -97,12 +104,17 @@ int main(int argc, char **argv, char **envp)
 		exit (errno);
 	}
 	pipe(fd);
+	printf("Acabo de iniciar el pipe\n");
+	printf("     El fd del pipe de lectura es %d\n", fd[FD_READ_END]);
+	printf("     El fd del pipe de escritura es %d\n", fd[FD_WRITE_END]);
 	if (pipe(fd) < 0)
 	{
 		perror("Pipe error. Pipe not created");
 		exit (errno);
 	}
 	pid = fork();
+	printf("****He generado el primer proceso\n");
+	printf("    el número pid del proceso Hijo1 generado es %d\n", pid);
 	if (pid < 0)
 	{
 		perror("Fork error. Child not created");
@@ -114,5 +126,5 @@ int main(int argc, char **argv, char **envp)
 	wait(&status);
 	freepointers(pipex);
 	atexit(leaks);
-	return 0;
+	return (0);
 }
